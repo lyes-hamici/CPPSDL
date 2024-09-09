392ea980de528ed146394887765b714541dfd7b7
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-09-09 15:01:29 +0200

    <feat>[Input]:<Added Inputs for sfml sdl & windows console>

--- src/Input/Input.cpp
+++ src/Input/Input.cpp
@@ -1,19 +1,20 @@
 #include "Input.hpp"
-#ifdef USE_WINDOWSCONSOLE
-	#include <windows.h>
-	#include <cctype>
-	#include <iostream>
-	std::map<std::string, int> Input::keys;
-	bool Input::wasPressed = false;
 
-	void Input::initialize()
-	{
-		Input::keys["Left"] = VK_LEFT;
-		Input::keys["Right"] = VK_RIGHT;
-		Input::keys["Up"] = VK_UP;
-		Input::keys["Down"] = VK_DOWN;
-		Input::keys["Escape"] = VK_ESCAPE;
-		Input::keys["Return"] = VK_RETURN;
+bool Input::wasPressed = false;
+std::string Input::keyPressed;
+
+#ifdef USE_WINDOWSCONSOLE
+
+std::map<std::string, int> Input::keys;
+
+void Input::initialize()
+{
+	Input::keys["Left"] = VK_LEFT;
+	Input::keys["Right"] = VK_RIGHT;
+	Input::keys["Up"] = VK_UP;
+	Input::keys["Down"] = VK_DOWN;
+	Input::keys["Escape"] = VK_ESCAPE;
+	Input::keys["Return"] = VK_RETURN;
 	}
 
 	bool Input::getPressed(char character){
@@ -34,11 +35,12 @@
 		if (name.length() > 1 && Input::keys.contains(name))
 		{
 			state = GetKeyState(Input::keys[name]);
-			if (!(Input::wasPressed) && state & WM_KEYDOWN)
+			if (!(Input::wasPressed) && (state & WM_KEYDOWN))
 			{
+				Input::keyPressed = name;
 				return Input::wasPressed = true;
 			}
-			else if (Input::wasPressed && state & WM_KEYUP)
+			else if ((Input::wasPressed) && !(state & WM_KEYDOWN) && (Input::keyPressed == name))
 			{
 				return Input::wasPressed = false;
 			}
@@ -49,16 +51,77 @@
 #endif
 #ifdef USE_SDL
 
-#endif
-#ifdef USE_SFML
-	#include <map>
-	#include <string>
-	#include <SFML/Window/Keyboard.hpp>
-	#include <cctype>
-	#include <iostream>
-	#include <thread> // pour std::this_thread::sleep_for
-	#include <chrono> // pour std::chrono::milliseconds
+	std::map<std::string, SDL_KeyCode> Input::keys;
 
+	void Input::initialize()
+	{
+		// Testing Block
+		SDL_Init(SDL_INIT_EVERYTHING);
+		SDL_Window *window = SDL_CreateWindow("Ma Fenêtre SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_SHOWN);
+
+		if (window == nullptr)
+		{
+			std::cerr << "Erreur lors de la création de la fenêtre: " << SDL_GetError() << std::endl;
+			SDL_Quit();
+		}
+
+		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		if (renderer == nullptr)
+		{
+			std::cerr << "Erreur lors de la création du renderer: " << SDL_GetError() << std::endl;
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+		}
+		// End Testing Block
+		Input::keys["Left"] = SDLK_LEFT;
+		Input::keys["Right"] = SDLK_RIGHT;
+		Input::keys["Up"] = SDLK_UP;
+		Input::keys["Down"] = SDLK_DOWN;
+		Input::keys["Escape"] = SDLK_ESCAPE;
+		Input::keys["Return"] = SDLK_RETURN;
+		Input::keys["r"] = SDLK_r;
+	}
+
+	bool Input::getPressed(char character)
+	{
+		return false;
+	}
+
+	bool Input::getPressed(std::string name)
+	{
+		if (name.empty())
+		{
+			return false;
+		}
+
+		if (Input::keys.contains(name))
+		{
+			const Uint8 *state = SDL_GetKeyboardState(NULL);
+			SDL_Scancode scancode = SDL_GetScancodeFromKey(Input::keys[name]);
+			SDL_PumpEvents();
+			if (state[scancode] && !Input::wasPressed)
+			{
+				Input::wasPressed = true;
+				Input::keyPressed = Input::keys[name];
+				SDL_PumpEvents();
+				return true;
+			}
+			else if (!state[scancode] && Input::wasPressed && Input::keyPressed == InputSDL::keys[name])
+			{
+				Input::wasPressed = false;
+				SDL_PumpEvents();
+				return false;
+			}
+			else
+			{
+				SDL_PumpEvents();
+				return false;
+			}
+		}
+	}
+#endif
+
+#ifdef USE_SFML
 	std::map<std::string, sf::Keyboard::Key> Input::keys;
 
 	void Input::initialize()
@@ -93,6 +156,8 @@
 		// Sinon, traiter comme une touche de caractère unique
 	}
 #endif
+
+
 // int main()
 // {
 //     Input::initialize();
