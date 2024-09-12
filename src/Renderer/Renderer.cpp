a9797783a0892526c72800bf8d8aace57c62b82d
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-09-12 10:24:17 +0200

    <feat>[Renderer]:<added background to the SDL game>

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -52,7 +52,7 @@ SDL_Renderer* Renderer::renderer;
 void Renderer::initialize()
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    // TTF_Init();
+    TTF_Init();
     IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG | IMG_INIT_AVIF);
     Renderer::window = SDL_CreateWindow("Ma Fenêtre SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 700, SDL_WINDOW_SHOWN);
 
@@ -78,21 +78,30 @@ void Renderer::destroy()
 
 void Renderer::clear()
 {
-
+    SDL_SetRenderDrawColor(Renderer::renderer, 0, 0, 0, 255);
+    SDL_RenderClear(Renderer::renderer);
 }
 void Renderer::loadTextures()
 {
-    std::string path = "Images/";
     SDL_Surface *surf;
+
     for (int i = 2; i < 65537; i *= 2)
     {
-        std::string filePath = path + std::to_string(i) + ".png";
+        std::string filePath = "Images/" + std::to_string(i) + ".png";
         surf = IMG_Load(filePath.c_str());
         Renderer::images[std::to_string(i)] = SDL_CreateTextureFromSurface(Renderer::renderer, surf);
         SDL_FreeSurface(surf);
     }
+
     surf = IMG_Load("Images/0.png");
     Renderer::images["0"] = SDL_CreateTextureFromSurface(Renderer::renderer, surf);
+    surf = IMG_Load("Images/Background.png");
+    Renderer::images["Background"] = SDL_CreateTextureFromSurface(Renderer::renderer, surf);
+    surf = IMG_Load("Images/Text.png");
+    Renderer::images["Text"] = SDL_CreateTextureFromSurface(Renderer::renderer, surf);
+    surf = IMG_Load("Images/Grid_background.png");
+    Renderer::images["Grid_background"] = SDL_CreateTextureFromSurface(Renderer::renderer, surf);
+   
     SDL_FreeSurface(surf);
 }
 
@@ -102,10 +111,7 @@ void Renderer::loadFont()
 }
 
 void Renderer::draw()
-{
-    SDL_SetRenderDrawColor(Renderer::renderer, 255, 0, 0, 255); // Set background color
-    SDL_RenderClear(Renderer::renderer);                        // Clear the screen
-
+{                        // Clear the screen
     int xPosition = 0;     // Initial x position
     int yPosition = 50;    // Fixed y position (line)
     int imageWidth = 50;  // Width of each image
@@ -124,40 +130,47 @@ void Renderer::draw()
 }
 void Renderer::draw(int (&board)[4][4])
 {
-    SDL_SetRenderDrawColor(Renderer::renderer, 255, 0, 0, 255); // Set background color
-    SDL_RenderClear(Renderer::renderer);                        // Clear the screen
+    SDL_RenderCopy(Renderer::renderer, Renderer::images["Background"], NULL, NULL);
+    
+    int tileSize = 80;                              
+    int spacing = 8;                                
+    int gridWidth = (4 * tileSize) + (3 * spacing);  
+    int gridHeight = (4 * tileSize) + (3 * spacing); 
 
-    int tileSize = 100;                              // Size of each tile (width and height)
-    int spacing = 10;                                // Spacing between the tiles
-    int gridWidth = (4 * tileSize) + (3 * spacing);  // Total width of the grid
-    int gridHeight = (4 * tileSize) + (3 * spacing); // Total height of the grid
-
-    // Centering the grid on a 600x700 screen
     int startX = (600 - gridWidth) / 2;
     int startY = (700 - gridHeight) / 2;
+
+    SDL_Rect gridBackgroundRect = {startX - 40, startY - 40, gridWidth + 80, gridHeight + 80};
+    SDL_SetTextureAlphaMod(Renderer::images["Grid_background"], 200);
+    SDL_RenderCopy(Renderer::renderer, Renderer::images["Grid_background"], NULL, &gridBackgroundRect);
+    
+    SDL_Rect textRect = {startX - 40, 20, gridWidth + 80, 100};
+    SDL_Rect scoreRect = {startX - 40, gridHeight + startY + 60, gridWidth + 80, 100};
+    SDL_RenderCopy(Renderer::renderer, Renderer::images["Text"], NULL, &textRect);
+    SDL_RenderCopy(Renderer::renderer, Renderer::images["Text"], NULL, &scoreRect);
 
     for (int row = 0; row < 4; ++row)
     {
         for (int col = 0; col < 4; ++col)
         {
-            int value = board[row][col]; // Get the value from the grid
+            int value = board[row][col]; 
 
-            SDL_Rect dstRect = {
-                startX + col * (tileSize + spacing), // x position
-                startY + row * (tileSize + spacing), // y position
-                tileSize,                            // width
-                tileSize                             // height
+            SDL_Rect tilesRect = {
+                startX + col * (tileSize + spacing),
+                startY + row * (tileSize + spacing), 
+                tileSize,                            
+                tileSize                             
             };
 
-            std::string textureKey = std::to_string(value); // Use the grid value to get the corresponding texture
+            std::string textureKey = std::to_string(value);
             if (Renderer::images.find(textureKey) != Renderer::images.end())
             {
-                SDL_RenderCopy(Renderer::renderer, Renderer::images[textureKey], NULL, &dstRect);
+                SDL_RenderCopy(Renderer::renderer, Renderer::images[textureKey], NULL, &tilesRect);
             }
         }
     }
 
-    SDL_RenderPresent(Renderer::renderer); // Display rendered content
+    SDL_RenderPresent(Renderer::renderer);
 }
 
 void Renderer::drawRow(void *row, int size)
