d613fad6d0c5c7644872800cb737f1f6234af117
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-09-14 00:30:14 +0200

    <feat>[file]<create>[test]:<added a new test>

--- src/Game/FIle.cpp
+++ src/Game/FIle.cpp
@@ -5,9 +5,11 @@
 
 void File::saveScore(int score, std::string filePath)
 {
+    if (score < 0)
+        return;
+
     std::ofstream fout;
     fout.open(filePath);
-
     fout << std::to_string(score);
     fout.close();
 }
@@ -17,11 +19,12 @@ int File::loadScore(std::string filePath)
     std::fstream fio = std::fstream(filePath, std::ios::in);
     std::string line;
 
-    if (!fio) {return 0;}
-    auto fileLenght = (int)std::filesystem::file_size(filePath);
-    if (fileLenght < 1) {return 0;}
+    if (!fio)
+        return 0; 
+    auto fileLength = (int)std::filesystem::file_size(filePath);
+    if (fileLength < 1)
+        return 0; 
 
-    auto inputStream = std::ostringstream() << fio.rdbuf();
-
-    return std::stoi(inputStream.str());
+    fio >> line;
+    return std::stoi(line);
 }
